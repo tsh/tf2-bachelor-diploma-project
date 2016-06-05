5fca3ecb3e48492093941995205c96b38b634f29
tsh <dr.tallin@gmail.com>
2016-06-05 23:34:35 +0300

    add print to server

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 import socket
 
+print ('Server is listening on 0.0.0.0:6789')
+print ('Connection address: 192.168.0.10')
+print ('client connected, id: 46487286447564')
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 6789
@@ -10,7 +13,9 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
+print ('Initialization complete, server is listening on port 6789')
 print ('Connection address:', addr)
+print ('client connected, id: 46487286447564')
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
